d7385626c95c0e744a9bb63aec5c66c7ea81961e
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2025-08-27 20:03:29 +0530

    Update Shops.js

--- frontend/src/components/Shops.js
+++ frontend/src/components/Shops.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ShopCard from './ShopCard';
-import config from './config';
+import config from '../config';
 
 function Shops() {
   const [shops, setShops] = useState([]);
