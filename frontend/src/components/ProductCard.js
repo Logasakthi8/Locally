8d6d83edda7007aa9f087be1825c06476ad4c32c
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2025-08-27 20:02:55 +0530

    Update ProductCard.js

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import config from './config';
+import config from '../config';
 
 function ProductCard({ product }) {
   const [isLiked, setIsLiked] = useState(false);
