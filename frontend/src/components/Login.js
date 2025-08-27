1867ae3a2c6f015a60204dd5c79e785ff7b532f4
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2025-08-27 20:02:11 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import config from './config';
+import config from '../config';
 
 function Login({ onLogin }) {
   const [mobile, setMobile] = useState('');
