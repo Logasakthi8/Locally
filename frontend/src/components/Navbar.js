3b85bf352be8936c856d8fff62de9bb849d01241
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2025-09-26 20:48:10 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,26 +5,25 @@ import config from '../config';
 function Navbar({ user, onLogout }) {
   const navigate = useNavigate();
 
+
   const handleLogout = async () => {
-    try {
-      await fetch(`${config.apiUrl}/logout`, {
-        method: 'POST',
-        credentials: 'include'
-      });
-      onLogout();
-      navigate("/"); // redirect to login
-    } catch (error) {
-      console.error("Error logging out:", error);
-    }
-  };
+  try {
+    await fetch(`${config.apiUrl}/logout`, {
+      method: 'POST',
+      credentials: 'include'
+    });
+    setUser(null);
+    navigate("/"); // redirect to login
+  } catch (error) {
+    console.error("Error logging out:", error);
+  }
+};
+
 
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <div className="logo-container" onClick={() => navigate(user ? '/shops' : '/')}>
-          <img src="/logo1.png" alt="Locally Logo" className="nav-logo" />
-          
-        </div>
+        <h1 onClick={() => navigate(user ? '/shops' : '/')} className="logo">Locally</h1>
         <div className="nav-links">
           {user ? (
             <>
