d15703e6ebcfd1900778215a930b92060f5e1a45
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2025-11-05 07:57:07 +0530

    Update Products.js

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
-import config from './config';
+import config from '../config';
 
 
 function Products() {
