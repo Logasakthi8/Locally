6eb758ea344a86923292fb940f2a90b936bb9285
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2025-11-05 07:50:53 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
-import './Navbar.css';
+
 
 function Navbar() {
   const { user, logout } = useAuth();
